f067d0b295c634367f7b5b2b0243da6bb2b66149
stacecadet17 <staceyabbott@stacey-abbotts-macbook.local>
2017-11-11 16:18:32 -0800

    first commit

--- Aging-people/ViewController.swift
+++ Aging-people/ViewController.swift
@@ -9,17 +9,34 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    let people = ["George", "Betty", "Fran", "Joe", "Helda", "Winifred", "Zed", "Sara", "Jeffy", "Abraham", "Anna", "Melinda"]
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+       
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+}
+
+extension ViewController: UITableViewDataSource {
+    
+    //determines how many rows we want
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return people.count
     }
-
-
+    
+    //determines what to put in each cell
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "PersonCell", for: indexPath)
+        cell.textLabel?.text = self.people[indexPath.row]
+        
+        //variable to determine a random number for age
+        let age = arc4random_uniform(95-5) + 5
+        //putting the random number in the text label in the row
+        cell.detailTextLabel?.text = "\(age) years old"
+        return cell
+    }
 }
 
